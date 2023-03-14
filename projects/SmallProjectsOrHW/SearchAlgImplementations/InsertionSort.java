b79703f33205cba76fce60f6f83c8452f6dc7a5e
John Doe <beyondthestars628@gmail.com>
2023-03-14 00:22:48 -0400

    insertionsort

--- projects/SmallProjectsOrHW/SearchAlgImplementations/InsertionSort.java
+++ projects/SmallProjectsOrHW/SearchAlgImplementations/InsertionSort.java
@@ -8,20 +8,44 @@ public class InsertionSort {
     static int[] array = new CollectionInitializer(10, "array").getArray();
     public static void main(String[] args){
         System.out.println(Arrays.toString(array));
-
+        InsertionSort(array, 0);
+        System.out.println("sorted array:");
+        System.out.println(Arrays.toString(array));
     }
-    public static void InsertionSort() {
-
+    public static void InsertionSort(int[] array, int index) {
+        if(index >= array.length){
+            insert(array, index - 1, index - 1);
+            return;
+        }
+        insert(array, index, index);
+        if(isSorted(array, index)) {
+            InsertionSort(array, index + 1);
+        } else{
+            while(!isSorted(array, index)) {
+                insert(array, index, index);
+            }
+            InsertionSort(array, index + 1);
+        }
     }
 
     public static void insert(int[] array, int maxIndex, int element) {
-        boolean condition1 = false, condition2 = false;
         for(int i = 0; i < maxIndex; i++){
-            if(condition1 && condition2){
-                array[i] = element;
+            if(array[element] < array[i]){
+                int elementholder = array[element];
+                array[element] = array[i];
+                array[i] = elementholder;
                 break;
             }
         }
+    }
+
+    public static boolean isSorted(int[] array, int index){
+        for(int i = 0; i < index; i++){
+            if(!(array[i] < array[i + 1])){
+                return false;
+            }
+        }
+        return true;
     }
 }
 
