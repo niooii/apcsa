14962d267ec66232733c8dff4c0b4a1d87ab0857
me laptop <beyondthestars628@gmail.com>
2023-03-14 21:05:52 -0400

    work on th thing yes

---
+++ projects/SmallProjectsOrHW/SearchAlgImplementations/MergeSort.java
@@ -0,0 +1,13 @@
+package SmallProjectsOrHW.SearchAlgImplementations;
+
+public class MergeSort {
+
+    int[] array = new CollectionInitializer(10, "array").getArray();
+
+    public static void main(String[] args){
+
+    }
+
+
+
+}
